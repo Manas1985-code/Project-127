d8c7211e30c5cd4dcffe131c70427afdc1c10aa2
Manas1985-code <75480122+Manas1985-code@users.noreply.github.com>
2021-09-03 16:50:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song1 = '';
+song2 = '';
+
+function preload()
+{
+    song1 = loadSound('');
+    song2 = loadSound('');
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
